4ccc70e583b287debfcc22a20e09feaeb619afa2
Rahul <27402666+rahulmathews@users.noreply.github.com>
2025-08-10 10:16:01 -0500

    build: improved release pipeline

--- .releaserc.js
+++ .releaserc.js
@@ -9,12 +9,55 @@ module.exports = {
     }
   ],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    ['@semantic-release/changelog', {
-      changelogFile: 'CHANGELOG.md',
-      changelogTitle: '# Changelog\n\nAll notable changes to this project will be documented in this file.\n\nThe format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),\nand this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).\n'
+    ['@semantic-release/commit-analyzer', {
+      preset: 'angular',
+      releaseRules: [
+        { type: 'feat', release: 'minor' },
+        { type: 'fix', release: 'patch' },
+        { type: 'docs', release: 'patch' },
+        { type: 'style', release: 'patch' },
+        { type: 'refactor', release: 'patch' },
+        { type: 'perf', release: 'patch' },
+        { type: 'test', release: 'patch' },
+        { type: 'build', release: 'patch' },
+        { type: 'ci', release: 'patch' },
+        { type: 'chore', release: 'patch' },
+        { type: 'revert', release: 'patch' },
+        { breaking: true, release: 'major' },
+      ],
+      parserOpts: {
+        noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+      }
     }],
+    ['@semantic-release/release-notes-generator', {
+      preset: 'angular',
+      presetConfig: {
+        types: [
+          { type: 'feat', section: '🚀 Features' },
+          { type: 'fix', section: '🐛 Bug Fixes' },
+          { type: 'docs', section: '📚 Documentation' },
+          { type: 'style', section: '💄 Styles' },
+          { type: 'refactor', section: '♻️ Code Refactoring' },
+          { type: 'perf', section: '⚡ Performance Improvements' },
+          { type: 'test', section: '✅ Tests' },
+          { type: 'build', section: '📦 Build System' },
+          { type: 'ci', section: '👷 CI Configuration' },
+          { type: 'chore', section: '🔧 Chores' },
+          { type: 'revert', section: '⏪ Reverts' }
+        ]
+      },
+      writerOpts: {
+         groupBy: 'type',
+         commitGroupsSort: 'title',
+         commitsSort: 'header',
+         noteGroupsSort: 'title',
+         reverse: true
+       }
+    }],
+   ['@semantic-release/changelog', {
+       changelogFile: 'CHANGELOG.md',
+       changelogTitle: '# Changelog\n\nAll notable changes to this project will be documented in this file.\n'
+     }],
     ['@semantic-release/github', {
       failTitle: false,
       labels: false,
@@ -22,60 +65,13 @@ module.exports = {
       successComment: false,
       failComment: false,
       assets: [
-        { path: 'dist', label: 'Distribution Files' },
-        { path: 'CHANGELOG.md', label: 'Changelog' }
-      ],
-      generateNotes: {
-        preset: 'angular',
-        presetConfig: {
-          types: [
-            { type: 'feat', section: '🚀 Features' },
-            { type: 'fix', section: '🐛 Bug Fixes' },
-            { type: 'docs', section: '📚 Documentation' },
-            { type: 'style', section: '💄 Styles' },
-            { type: 'refactor', section: '♻️ Code Refactoring' },
-            { type: 'perf', section: '⚡ Performance Improvements' },
-            { type: 'test', section: '✅ Tests' },
-            { type: 'build', section: '📦 Build System' },
-            { type: 'ci', section: '👷 CI Configuration' },
-            { type: 'chore', section: '🔧 Chores' },
-            { type: 'revert', section: '⏪ Reverts' }
-          ]
-        },
-        writerOpts: {
-          groupBy: 'type',
-          commitGroupsSort: 'title',
-          commitsSort: 'header',
-          noteGroupsSort: 'title'
-        }
-      }
+        { path: 'riders-server-dist.tar.gz', label: '🚀 Distribution Files' },
+        { path: 'CHANGELOG.md', label: '📋 Changelog' }
+      ]
     }],
     ['@semantic-release/git', {
       assets: ['CHANGELOG.md', 'package.json'],
       message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     }],
-  ],
-  preset: 'angular',
-  releaseRules: [
-    { type: 'feat', release: 'minor' },
-    { type: 'fix', release: 'patch' },
-    { type: 'docs', release: 'patch' },
-    { type: 'style', release: 'patch' },
-    { type: 'refactor', release: 'patch' },
-    { type: 'perf', release: 'patch' },
-    { type: 'test', release: 'patch' },
-    { type: 'build', release: 'patch' },
-    { type: 'ci', release: 'patch' },
-    { type: 'chore', release: 'patch' },
-    { type: 'revert', release: 'patch' },
-    { breaking: true, release: 'major' },
-  ],
-  parserOpts: {
-    noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
-  },
-  writerOpts: {
-    groupBy: 'type',
-    commitGroupsSort: 'title',
-    commitsSort: 'header',
-  },
+  ]
 };
